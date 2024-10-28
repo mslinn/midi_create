bde9be73f633f873dd38bcbc646688d3274abae5
Mike Slinn <mslinn@mslinn.com>
2024-10-28 09:00:21 -0400

    Middle C sorted for realz

--- lib/midi_create/create.rb
+++ lib/midi_create/create.rb
@@ -40,12 +40,16 @@ module MidiCreate
       # delta time length of a single quarter note.
       track.events << ProgramChange.new(0, 1, 0)
       quarter_note_length = seq.note_to_delta('quarter')
-      c4 = 69 + 3 # There is probably a more elegant way of specifying middle C (C4)
+
+      # There is probably a more elegant way of specifying middle C,
+      # which shows as C3 in Pro Tools and Ableton Live.
       # See https://studiocode.dev/resources/midi-middle-c/
       # See https://stackoverflow.com/a/69182634/553865
+      c3 = 60
+
       [0, 2, 4, 5, 7, 9, 11, 12].each do |offset|
-        track.events << NoteOn.new(0, c4 + offset, 127, 0)
-        track.events << NoteOff.new(0, c4 + offset, 127, quarter_note_length)
+        track.events << NoteOn.new(0, c3 + offset, 127, 0)
+        track.events << NoteOff.new(0, c3 + offset, 127, quarter_note_length)
       end
     end
 
